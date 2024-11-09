583cd808fe8bd2567d56c2fddcb407e1ceff99eb
Amaam675 <amaanzesiddiqui@gmail.com>
2024-11-09 23:23:22 +0530

    Update checkoutGit.groovy

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,8 +1,8 @@
 def call(String gitUrl, String gitBranch) {
     checkout([
         $class: 'GitSCM',
-        branches: [[name: refs/heads/${gitBranch}]]
-        userRemoteConfigs: [[url: gitUrl]]
+        branches: [[name: refs/heads/${gitBranch}]],
+        userRemoteConfigs: [[url: gitUrl]],
 ])
 }
 
