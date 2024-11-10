70d52e8f2a2cac5820ae4f5ce8b8cb803191801c
Amaam675 <amaanzesiddiqui@gmail.com>
2024-11-10 15:06:43 +0530

    Update checkoutSonar.groovy

--- vars/checkoutSonar.groovy
+++ vars/checkoutSonar.groovy
@@ -1,6 +1,6 @@
 def call(String SCANNER_HOME){
   withSonarQubeEnv('sonar-server'){
-    sh '''$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName youtube -Dsonar.projectKey youtube'''
+    sh '''$SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube -Dsonar.projectKey=youtube'''
   }
 
 }
